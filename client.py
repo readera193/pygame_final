d492732ad2cdd5d1dd507f3ac41d7283f8b4d7da
readera193 <readera193@gmail.com>
2020-12-28 02:35:17 +0800

    移除註解

--- client.py
+++ client.py
@@ -145,25 +145,6 @@ def main(user_id):
         redrawWindow(win, game, player)
 
 def menu_screen():
-    """
-    run = True
-    clock = pygame.time.Clock()
-
-    while run:
-        clock.tick(60)
-        win.fill((128, 128, 128))
-        font = pygame.font.SysFont("comicsans", 60)
-        text = font.render("Click to Play!", 1, (255,0,0))
-        win.blit(text, (100,200))
-        pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                run = False
-    """
     user_id = login.main()
     main(user_id)
 
